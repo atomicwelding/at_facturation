40b7ee9bcc5e2d64ef6941e9b754da5b95161419
weld <contact@weldev.tech>
2019-08-12 17:53:04 +0200

    ajouter_une_facture_UI UI complete

--- src/view/ajouter_facture_UI.py
+++ src/view/ajouter_facture_UI.py
@@ -1,67 +1,65 @@
-""" Ouvre une nouvelle fenêtre pour ajouter une facture
+""" Ajouter une nouvelle facture au livre des recettes
+"""
+
+""" J'ai utilisé les méthodes "place()" plutôt que grid() ou pack() car ces dernières me semblent approximatives.
+Le résultat en est un code un peu moins joli, mais qui me permet de placer au pixel près tous mes widgets.
 """
 from tkinter import *
 from tkinter.ttk import *
 
+from validating_entry import * 
+
 def init(master):
     window = Toplevel(master)
-    window.geometry("700x250+300+300")
+    window.geometry("545x175+300+300")
     window.title("Ajouter une nouvelle facture")
 
 #########   
-   
+
     # id
-    Label(window, text="ID :").grid(row=0, column=0, pady=5)
-    Entry(window).grid(row=0, column=1, pady=5)
+    Label(window, text="ID :").place(x=0, y=0, height=20)
+    MaxLengthEntry(window, maxlength=4).place(x=25, y=0, height=21, width=48)
 
 #########
 
     # nom
-    Label(window, text="Nom :").grid(row=2, column=0, pady=10)
-    Entry(window).grid(row=2, column=1, pady=5)
+    Label(window, text="Nom :").place(x=0, y=40, height=20)
+    Entry(window).place(x=40, y=40, height=21, width=100)
 
     # adresse
-    Label(window, text="Voie :").grid(row=2, column=3, pady=10)
-    Entry(window).grid(row=2, column=4, pady=10)
-
+    Label(window, text="Voie :").place(x=140, y=40, height=20)
+    Entry(window).place(x=179, y=40, height=21, width=100)
+    
     # code postal
-    Label(window, text="CP:").grid(row=2, column=5, pady=10)
-    Entry(window).grid(row=2, column=6, pady=10)
+    Label(window, text="CP :").place(x=278, y=40, height=20)
+    MaxLengthEntry(window, maxlength=5).place(x=307, y=40, height=21, width=60)
 
     # ville
-    Label(window, text="Ville :").grid(row=2, column=7, pady=10)
-    Entry(window).grid(row=2, column=8, pady=10)
+    Label(window, text="Ville :").place(x=370, y=40, height=20)
+    Entry(window).place(x=409, y=40, height=21, width=120)
 
 #########
 
     # date
+    Label(window, text="Date :").place(x=0, y=80, height=20)
+    MaxLengthEntry(window, maxlength=2).place(x=40, y=80, height=21, width=28)
 
+    Label(window, text="/").place(x=67, y=75, height=30)
+    MaxLengthEntry(window, maxlength=2).place(x=77, y=80, height=21, width=28)
 
-    Label(window, text="date :").grid(row=3, column=0, pady=10)
-    Entry(window, width=2).grid(row=3, column=1, pady=10)
+    Label(window, text="/").place(x=105, y=75, height=30)
+    MaxLengthEntry(window, maxlength=4).place(x=115, y=80, height=21, width=52)
 
-    Label(window, text="/").grid(row=3, column=2, pady=10)
-    Entry(window, width=2).grid(row=3, column=2, pady=10)
-
-    Label(window, text="/").grid(row=3, column=4, pady=10)
-    Entry(window, width=2).grid(row=3, column=5, pady=10)
-
-
-
-    # tarif
-    # Label(window, text="tarif :").grid(row=4, column=0, pady=5)
-    # Entry(window).grid(row=4, column=1, pady=5)
+    # tarif horaire
+    Label(window, text="€/h :").place(x=220, y=80, height=20)
+    MaxLengthEntry(window, maxlength=4).place(x=250, y=80, height=21, width=52)
 
     # quantité
-    # Label(window, text="quantité :").grid(row=5, column=0, pady=5)
-    # Entry(window).grid(row=5, column=1, pady=5)
-
-    # nature de la prestation
-    # Label(window, text="nature :").grid(row=6, column=0, pady=5)
-    # Entry(window).grid(row=6, column=1, pady=5)
+    Label(window, text="Qté :").place(x=305, y=80, height=20)
+    MaxLengthEntry(window, maxlength=4).place(x=340, y=80, height=21, width=52)
 
     # valider
-    #Button(window, text="Valider").grid(row=7, column=0, pady=5)
+    Button(window, text="Valider").place(x=450, y=120, height=21)
 
     
     window.mainloop()
---
+++ src/view/validating_entry.py
@@ -0,0 +1,43 @@
+""" Fredrik Lundh, " June 30, 1998 " @ http://effbot.org/zone/tkinter-entry-validate.htm
+"""
+
+from tkinter import *
+
+class ValidatingEntry(Entry):
+    # base class for validating entry widgets
+
+    def __init__(self, master, value="", **kw):
+        # apply(Entry.__init__, (self, master), kw)
+        Entry.__init__(*(self, master), kw)
+        self.__value = value
+        self.__variable = StringVar()
+        self.__variable.set(value)
+        self.__variable.trace("w", self.__callback)
+        self.config(textvariable=self.__variable)
+
+    def __callback(self, *dummy):
+        value = self.__variable.get()
+        newvalue = self.validate(value)
+        if newvalue is None:
+            self.__variable.set(self.__value)
+        elif newvalue != value:
+            self.__value = newvalue
+            self.__variable.set(self.newvalue)
+        else:
+            self.__value = value
+
+    def validate(self, value):
+        # override: return value, new value, or None if invalid
+        return value
+
+class MaxLengthEntry(ValidatingEntry):
+
+    def __init__(self, master, value="", maxlength=None, **kw):
+        self.maxlength = maxlength
+        # apply(ValidatingEntry.__init__, (self, master), kw)
+        ValidatingEntry.__init__(*(self, master), kw)
+
+    def validate(self, value):
+        if self.maxlength is None or len(value) <= self.maxlength:
+            return value
+        return None # new value too long
