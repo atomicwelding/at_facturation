6adb2b4f13ae8734c26f59fc5fc0ad5730d787b0
weld <contact@weldev.tech>
2019-08-15 09:54:16 +0200

    script create db

---
+++ src/controller/create_user_table.py
@@ -0,0 +1,32 @@
+""" Quick script to create associated tables
+"""
+import sqlite3 as s
+
+db = s.connect("../model/database.db")
+
+cursor = db.cursor()
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS utilisateurs(
+        id_client INTEGER,
+        nom TEXT,
+        voie TEXT,
+        cp INTEGER, 
+        ville TEXT
+    )''')
+
+db.commit()
+
+cursor = db.cursor()
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS factures(
+        id_facture INTEGER,
+        id_client INTEGER,
+        date TEXT,
+        taux_horaire INTEGER,
+        qte INTEGER,
+        reglement TEXT
+    )
+''')
+db.commit()
+
+db.close()
